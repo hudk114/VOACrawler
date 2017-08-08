4cab6bca6e9c56ac4188667230224c8f60b308ad
Hu DeKai(焦点) <dekaihu@sohu-inc.com>
2017-08-08 21:39:31 +0800

    eslint modify

---
+++ Node/test.js
@@ -0,0 +1,23 @@
+/**
+ * Created by dekaihu on 2017/8/8.
+ */
+const b =1;
+const c = 'saf';
+
+const a = {
+    b,
+    c() {
+        return `asdf${c}`;
+    }
+};
+
+const dd = function dd() {
+    if(c != 1) {
+        const f = function f() {
+            return null;
+        };
+    }
+    return null;
+};
+
+const ee = () => { dd();};
